c4c049e365f5e641923647755b4d0cf14c5ca38d
abhijith-offl <163877610+abhijith-offl@users.noreply.github.com>
2024-03-26 20:17:54 +0530

    Form Details
    DOM(Event addition in the submit button)

---
+++ script.js
@@ -0,0 +1,9 @@
+function final(){
+    let fname = document.getElementById("firstname").value;
+    let mname = document.getElementById("middlename").value;
+    let lname = document.getElementById("lastname").value;
+    let mail = document.getElementById("email").value;
+    let home = document.getElementById("address").value;
+
+console.log(fname,mname,lname,mail,home)
+}
